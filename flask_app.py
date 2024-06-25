3067b7f5c0ca21b0a4ba3237283ce1c8e64faffa
lxnd <lxnd@protonmail.com>
2024-06-25 09:01:13 +0200

    update: return a templated page

--- flask_app.py
+++ flask_app.py
@@ -6,5 +6,50 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return leequotes.random()
+    quote = leequotes.random()\
+        .replace("\\n", "<br>")\
+        # .replace(". ", ".<br>")
+
+    return f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Quote of the Day</title>
+        <style>
+            body, html {{
+                height: 100%;
+                margin: 0;
+                font-family: Verdana, sans-serif, system-ui;
+            }}
+            
+            body {{
+                background-color: #EBEBEB; /* Wheat color for a paper-like background */
+                color: #333; /* Dark gray color for text which is easier on the eyes */
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                padding: 20px;
+                text-align: center;
+            }}
+
+    
+            .centered-quote {{
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                text-align: center;
+                font-size: calc(16px + 2vmin);
+            }}
+        </style>
+    </head>
+    <body>
+        <div class="centered-quote">
+            {quote}
+        </div>
+    </body>
+    </html>
+    """
 
